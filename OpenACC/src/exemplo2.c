60b9c7675bd5109d5f4b0f110184e3834a7883a2
Andre Leon Sampaio Gradvohl <gradvohl@ft.unicamp.br>
2020-09-22 19:13:51 +0000

    Atualizacao de exemplos

--- OpenACC/src/exemplo2.c
+++ OpenACC/src/exemplo2.c
@@ -1,11 +1,31 @@
+/**
+ * Programa exemplo em OpenACC.
+ */ 
+#include <stdio.h>
 #include <stdlib.h>
+#include <openacc.h>
 
+/**
+ * Funcao para multiplicar um escalar a um numero 
+ * e acumular nas posicoes de um vetor.
+ */ 
 void saxpy(int n, float a, float *x, float *restrict y)
 {
-
     #pragma acc parallel loop
     for (int i = 0; i < n; ++i)
         y[i] = a * x[i] + y[i];
+}
+
+void imprime10(float *y)
+{
+  register unsigned int i;
+
+  printf("{");
+  for (i=0; i<9; i++)
+    printf("%.2f, ",y[i]);
+
+  printf("%.2f}\n",y[i]);
+  return;
 }
 
 int main(int argc, char **argv) {
@@ -18,6 +38,12 @@ int main(int argc, char **argv) {
         x[i] = 2.0f;
         y[i] = 1.0f;
     }
+    printf("Antes: ");
+    imprime10(y);
+
     saxpy(N, 3.0f, x, y);
+
+    printf("Depois: ");
+    imprime10(y);
     return 0;
 }
